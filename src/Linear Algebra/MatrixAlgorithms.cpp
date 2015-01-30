ba3fd651b7683255c33a2a8eed8420b9326da509
frmsaul <frmsaul@gmail.com>
2015-01-30 13:59:52 -0800

    Added Matrix Algorothms bugs

---
+++ src/Linear Algebra/MatrixAlgorithms.cpp
@@ -0,0 +1,12 @@
+#include "MatrixAlgorithms.h"
+#include "Mat.h"
+#include <stdio.h>
+
+#define DEBUG_MAT_ALGO
+
+#ifndef DEBUG_MAT_ALGO
+int main(){
+  printf("DEBUGING Matrix Algorithm /n");
+  return 0;
+}
+#endif
---
+++ src/Linear Algebra/MatrixAlgorithms.h
@@ -0,0 +1,22 @@
+#ifndef _H_MATRIX_ALGORITHMS
+#define _H_MATRIX_ALGORITHMS
+#include "Mat.h"
+
+typedef float Real;
+
+Mat row_reduce(const Mat& M);
+Mat swop_rows(const Mat& M, int row1, int row2);
+Mat multiply_row(const Mat& M, int row, Real alph);
+Mat multiply_and_add_row(const Mat& M, int row_src, int row_dest, Real alph);
+
+Mat Tr(const Mat& M); //Transpose
+Mat Inverse(const Mat& M);
+Mat pseudo_inverse();
+
+void swop_rows(Mat* );
+
+Mat get_identity(int n);
+Mat get_zeros(int n);
+Mat random_matrix();
+
+#endif
